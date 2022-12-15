90fb521c9a6d5aabcd953aeec656163346828140
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2022-12-15 11:53:45 +0100

    did some coding

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use std::{
     collections::HashMap,
-    process::exit,
-    env::args,
+    env::args
 };
 // static const
 const RED: &'static str = "\x1b[1;31m";
@@ -14,7 +13,7 @@ pub struct ArgParser {
     settings: Settings,
     other_args: Vec<String>,
     args: HashMap<String, Option<Vec<String>>>,
-    private: Private
+    _private: Private
 }
 
 // parser impl
@@ -25,131 +24,96 @@ impl ArgParser {
             settings: Settings::new(),
             other_args: Vec::new(),
             args: HashMap::new(),
-            private: Private::new()
+            _private: Private::new()
         }
     }
     pub fn add_arg(&mut self, arg: &str, multiple_allowed: bool, require_next_arg: bool) {
-        if multiple_allowed {self.private.multi.push(arg.clone().to_string())}
-        if !require_next_arg {self.private.no_next.push(arg.clone().to_string())}
-        self.private.arg_keys.push(arg.to_string())
+        if multiple_allowed {self._private.multi.push(arg.clone().to_string())}
+        if !require_next_arg {self._private.no_next.push(arg.clone().to_string())}
+        self._private.arg_keys.push(arg.to_string())
     }
-    pub fn parse(&mut self) {
+    pub fn parse(&mut self) -> Result<(), String> {
         let argv: Vec<String> = args().skip(1).collect();
         let argc = argv.len();
         // if no args
+        // if no args are provided but allowed
         if argc == 0 {
-            // if no args are provided but allowed
             if self.settings.allow_no_args {
-                match self.settings.event_functions.no_args_allowed {
-                    Some(func) => func(),
-                    None => {}
+                return match self.settings.event_functions.no_args_allowed {
+                    Some(func) => Ok(func()),
+                    None => Err(format!("{}ArgParse Errror {}Provide at least one argument", RED, RESET).to_string())
                 }
             // no args provided but allowed
             } else {
-                match self.settings.event_functions.no_arg_err {
-                    Some(func) => func(),
-                    None => {
-                        eprintln!("{}ArgParse Errror {}{}", RED, RESET, self.settings.event_phrases.no_arg_err);
-                        exit(1);
-                    }
+                return match self.settings.event_functions.no_arg_err {
+                    Some(func) => Ok(func()),
+                    None => Err(format!("{}ArgParse Errror {}{}", RED, RESET, self.settings.event_phrases.no_arg_err))
                 }
             }
-        // if some args
         } else {
             let mut arg_index: usize = 0;
             while arg_index < argc {
                 // check if arg is valid
-                if ArgParser::contains(self.private.arg_keys.clone(), argv[arg_index].clone()) != -1 {
+                if ArgParser::contains(&self._private.arg_keys, &argv[arg_index]) != -1 {
                     // if not yet done or multiple allowed
-                    if ArgParser::contains(self.private.multi.clone(), argv[arg_index].clone()) != -1 || ArgParser::contains(self.private.multi_cmp.clone(), argv[arg_index].clone()) == -1 {
-                        self.private.multi_cmp.push(argv[arg_index].clone());
+                    if ArgParser::contains(&self._private.multi, &argv[arg_index]) != -1 || ArgParser::contains(&self._private.multi_cmp, &argv[arg_index]) == -1 {
+                        self._private.multi_cmp.push(argv[arg_index].clone());
                         // if no next arg is required
-                        if ArgParser::contains(self.private.no_next.clone(), argv[arg_index].clone()) != -1 {
+                        if ArgParser::contains(&self._private.no_next, &argv[arg_index]) != -1 {
                             self.args.insert(argv[arg_index].clone(), None);
                         // if next arg is required
                         } else {
                             // if there is a next elem
                             if arg_index+1 <= argc-1 {
-                                println!("Valid next => ( argc:{}, arg_index:{} )", argc, arg_index);
+                                if self.args.contains_key(&argv[arg_index]) {
+                                    match self.args[&argv[arg_index]].clone() {
+                                        Some(mut vec) => {
+                                            vec.push(argv[arg_index+1].clone());
+                                            // replace vec
+                                            let _ = &&self.args.remove(&argv[arg_index]);
+                                            self.args.insert(argv[arg_index].clone(), Some(vec));
+                                        },
+                                        None => {}
+                                    }
+                                } else {
+                                    let vec: Vec<String> = vec![argv[arg_index+1].clone()];
+                                    self.args.insert(argv[arg_index].clone(), Some(vec));
+                                }
+                                // skip
+                                arg_index += 1;
                             // no next arg err
                             } else {
                                 match self.settings.event_functions.missing_arg_err {
                                     Some(func) => func(),
-                                    None => {
-                                        eprintln!("{}ArgParse Errror \'{}{}{}\'{} {}", RED, RESET, argv[arg_index], RED, RESET, self.settings.event_phrases.missing_arg_err);
-                                        exit(1);
-                                    }
+                                    None => return Err::<(), String>(format!("{}ArgParse Errror \'{}{}{}\'{} {}", RED, RESET, &argv[arg_index], RED, RESET, self.settings.event_phrases.missing_arg_err))
                                 }
                             }
                         }
+                    } else {
+                        match self.settings.event_functions.multi_arg_err {
+                            Some(func) => func(),
+                            None => return Err::<(), String>(format!("{}ArgParse Errror \'{}{}{}\'{} {}", RED, RESET, argv[arg_index], RED, RESET, self.settings.event_phrases.multi_arg_err))
+                        }
                     }
-                // invalid argument
+                // invalid arg
                 } else {
                     if self.settings.allow_invalid_args {
                         self.other_args.push(argv[arg_index].clone());
                     } else {
                         match self.settings.event_functions.invalid_arg_err {
                             Some(func) => func(),
-                            None => {
-                                eprintln!("{}ArgParse Errror \'{}{}{}\'{} {}", RED, RESET, argv[arg_index], RED, RESET, self.settings.event_phrases.invalid_arg_err);
-                                exit(1);
-                            }
+                            None => return Err::<(), String>(format!("{}ArgParse Errror \'{}{}{}\'{} {}", RED, RESET, argv[arg_index], RED, RESET, self.settings.event_phrases.invalid_arg_err))
                         }
                     }
                 }
-                // End: inc (Do not touch!)
+                // inc (DO NOT TOUCH!)
                 arg_index += 1;
-            }
+            };
         }
-    }
-    // @test
-    fn testing(self) {
-        println!("\n\x1b[1;31m<---------@testing---------->\x1b[0m");
-        println!("\x1b[1;33m///// pub /////\x1b[0m");
-        // *@other_args
-        print!("other_args => [ ");
-        for item in self.other_args.iter() {
-            print!("{} ", item);
-        }
-        println!("];");
-        // *@args
-        println!("args => [ ");
-        for (arg, key) in &self.args {
-            print!("  {} => [ ", arg);
-            match key {
-                Some(item) => {
-                    for elem in item.iter() {
-                        print!("{} ", elem);
-                    }
-                    println!("];");
-                },
-                None => println!("None ];")
-            }
-        }
-        println!("];");
-        println!("\x1b[1;33m///// privd /////\x1b[0m");
-        // *@arg_keys
-        print!("arg_keys => [ ");
-        for item in self.private.arg_keys.iter() {
-            print!("{} ", item);
-        }
-        println!("];");
-        // *@multi
-        print!("multi => [ ");
-        for item in self.private.multi.iter() {
-            print!("{} ", item);
-        }
-        println!("];");
-        // *@multi_cmp
-        print!("multi_cmp => [ ");
-        for item in self.private.multi_cmp.iter() {
-            print!("{} ", item);
-        }
-        println!("];");
-        println!("\x1b[1;31m<-------end @testing-------->\x1b[0m");
+        Ok(())
     }
     // private
-    fn contains(target_vec: Vec<String>, item: String) -> i32 {
+    fn contains(target_vec: &Vec<String>, item: &String) -> i32 {
         for (pos, elem) in target_vec.iter().enumerate() {
             if item.eq(elem) {
                 let ret: i32 = pos as i32;
@@ -160,21 +124,7 @@ impl ArgParser {
     }
 }
 
-fn test() {
-    println!("Function ptr is called!!");
-}
-//////////////// Main /////////////////
-fn main() {
-    let mut parser = ArgParser::new();
-    parser.settings.allow_no_args = true;
-    parser.settings.allow_invalid_args = false;
-    parser.settings.event_functions.no_args_allowed = Some(test);
-    parser.add_arg("-h", true, true);
-    parser.parse();
-    parser.testing();
-}
-
-//////////////// Parser Settings /////////////////
+////// Parser Settings
 //// event phrases
 // event phrase struct
 struct EventPhrases {
@@ -234,7 +184,7 @@ impl Settings {
         }
     }
 }
-// private vars
+// _private vars
 struct Private{
     arg_keys: Vec<String>,
     multi: Vec<String>,
